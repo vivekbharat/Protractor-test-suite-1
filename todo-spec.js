ad358b9fa32958db66570060a459753266e8a7e3
Vivek Ravikumar <vr329@njit.edu>
2018-08-24 21:14:52 -0700

    1st test case completed

--- todo-spec.js
+++ todo-spec.js
@@ -6,8 +6,6 @@ describe("Protractor Element Demo", function() {
     element(by.model("first")).sendKeys("3");
     element(by.model("second")).sendKeys("5");
     element(by.id("gobutton")).click();
-    element(by.css("h2[class='ng-binding']"))
-      .getText()
-      .then(val => console.log(val));
+    expect(element(by.css("h2[class='ng-binding']")).getText()).toBe("8");
   });
 });
